55fabeacad9786fa747bb47cb4e8ff1577639838
Effekseer <effekseer@gmail.com>
2016-10-02 11:01:05 +0900

    fixed paths

--- Dev/Cpp/windows/Build.bat
+++ Dev/Cpp/windows/Build.bat
@@ -6,5 +6,5 @@ call "%VS120COMNTOOLS% \VsDevCmd.bat"
 msbuild EffekseerUnity.sln /t:rebuild /property:Configuration=Release /p:platform=x86
 msbuild EffekseerUnity.sln /t:rebuild /property:Configuration=Release /p:platform=x64
 
-copy Win32\Release\EffekseerUnity.dll ..\PluginProject\Assets\Plugins\x86\
-copy x64\Release\EffekseerUnity.dll ..\PluginProject\Assets\Plugins\x64\
+copy Win32\Release\EffekseerUnity.dll ..\..\Plugin\Assets\Plugins\x86\
+copy x64\Release\EffekseerUnity.dll ..\..\Plugin\Assets\Plugins\x64\
