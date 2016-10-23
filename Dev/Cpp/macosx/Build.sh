541726fa648920b0251615e64087095b092c93f1
ueshita <nalto32@gmail.com>
2016-10-23 16:59:12 +0900

    build for iOS/macOS

---
+++ Dev/Cpp/macosx/Build.sh
@@ -0,0 +1,18 @@
+rm -rf build
+rm libEffekseerUnity.a
+
+# build for macOS
+xcodebuild -target EffekseerUnity-Mac -configuration Release build
+
+# build for iOS (Device)
+xcodebuild -target EffekseerUnity-iOS -sdk iphoneos -configuration Release build
+
+# build for iOS (Simulator)
+xcodebuild -target EffekseerUnity-iOS -sdk iphonesimulator -configuration Release build
+
+# Create the Universal binary
+lipo -create build/Release-iphoneos/libEffekseerUnity.a build/Release-iphonesimulator/libEffekseerUnity.a -output libEffekseerUnity.a
+
+# Copy to PluginProject
+cp -rf build/Release/EffekseerUnity.bundle ../../Plugin/Assets/Plugins/
+cp libEffekseerUnity.a ../../Plugin/Assets/Plugins/iOS/
