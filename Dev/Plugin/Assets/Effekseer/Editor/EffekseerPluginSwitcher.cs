863d4da2be16a69f6aacbfb045c4970023581053
kou_yeung <kou_yeung@hotmail.com>
2017-02-26 22:55:38 +0900

    UnityWebGLPlugin更新
    5.5以降では emscripten バージョンが 1.36.x に更新されたためです。
    .bc は 1.36.7 でビルドし、Unity5.5 & Unity5.6 で動作確認しました
    (asm.jsのみ、wasmはまだ・・)
    
    Editor起動時及びインポート時に、Unityのバージョンによって使用する.bcを自動切り替えるようにしています。

---
+++ Dev/Plugin/Assets/Effekseer/Editor/EffekseerPluginSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+
+[InitializeOnLoad]
+public class EffekseerPluginSwitcher
+{
+    static readonly string tempFilePath = "Temp/EffekseerPluginSwitcher";
+
+#if UNITY_5_5_OR_NEWER
+    static readonly string bcPath = "WebGL/1.36.7-64bit/libEffekseerUnity.bc";
+#else
+    static readonly string bcPath = "WebGL/libEffekseerUnity.bc";
+#endif
+
+    static EffekseerPluginSwitcher()
+    {
+        Run();
+    }
+
+    static public void Run()
+    {
+        // １回だけ処理に制限します
+        if (!File.Exists(tempFilePath))
+        {
+            var importers = PluginImporter.GetAllImporters().Where(importer => importer.assetPath.Contains("libEffekseerUnity.bc"));
+
+            // 初回起動時にはまだ準備完了してなかったため、
+            // libEffekseerUnity.bc がヒットしなかった場合処理しない
+            if (importers.Count() > 0)
+            {
+                foreach (var importer in importers)
+                {
+                    importer.SetCompatibleWithPlatform(BuildTarget.WebGL, importer.assetPath.Contains(bcPath));
+                }
+                File.Create(tempFilePath);
+            }
+        }
+    }
+}
