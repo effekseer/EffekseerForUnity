17b2b23ae19fc4b316e576b9535be95b9d114b19
Effekseer <effekseer@gmail.com>
2017-10-31 21:48:51 +0900

    Updated documents

--- Help/build.bat
+++ Help/build.bat
@@ -4,3 +4,7 @@ set OPTS=-f markdown-auto_identifiers -t html5 --section-divs -c ../base.css
 for %%i in (Contents_en\*.md) do pandoc -s %%i %OPTS% -o Contents_en\%%~ni.html
 
 for %%i in (Contents_ja\*.md) do pandoc -s %%i %OPTS% -o Contents_ja\%%~ni.html
+
+echo doxygen(exe or bat)
+call doxygen.bat Doxyfile
+call doxygen.exe Doxyfile
