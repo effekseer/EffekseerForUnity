28ba8e25385d79e6e7ebad05f2bb741a19aecaa9
Effekseer <effekseer@gmail.com>
2018-09-11 21:22:07 +0900

    Fixed a flag based on other codes

--- Dev/Cpp/Device/EffekseerPluginDX11.cpp
+++ Dev/Cpp/Device/EffekseerPluginDX11.cpp
@@ -6,7 +6,7 @@ namespace EffekseerPlugin
 	EffekseerRenderer::Renderer* CreateRendererDX11(int squareMaxCount, bool reversedDepth, ID3D11Device* d3d11Device, ID3D11DeviceContext* d3d11Context)
 	{
 		// 深度テストの方法を切り替え
-		const D3D11_COMPARISON_FUNC depthFunc = (reversedDepth) ? D3D11_COMPARISON_GREATER : D3D11_COMPARISON_LESS;
+		const D3D11_COMPARISON_FUNC depthFunc = (reversedDepth) ? D3D11_COMPARISON_GREATER_EQUAL : D3D11_COMPARISON_LESS_EQUAL;
 
 		auto renderer = EffekseerRendererDX11::Renderer::Create(
 			d3d11Device, d3d11Context, squareMaxCount, depthFunc);
