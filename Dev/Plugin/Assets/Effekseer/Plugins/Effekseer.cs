97b15a3560c8fe851223b348317cf1a722cbe6cb
Effekseer <effekseer@gmail.com>
2018-11-03 13:57:36 +0900

    Added a switch flag

--- Dev/Plugin/Assets/Effekseer/Plugins/Effekseer.cs
+++ Dev/Plugin/Assets/Effekseer/Plugins/Effekseer.cs
@@ -139,10 +139,10 @@ namespace Effekseer
 	
 	internal static class Plugin
 	{
-		#if !UNITY_EDITOR && (UNITY_IPHONE || UNITY_WEBGL)
+#if !UNITY_EDITOR && (UNITY_IPHONE || UNITY_WEBGL || UNITY_SWITCH)
 			public const string pluginName = "__Internal";
-		#else
-			public const string pluginName = "EffekseerUnity";
+#else
+		public const string pluginName = "EffekseerUnity";
 		#endif
 
 		[DllImport(pluginName)]
