52a7d9169dab1d8feb6a615a76e450e1b19cfaa9
Effekseer <effekseer@gmail.com>
2019-07-04 23:13:29 +0900

    added tests

---
+++ Dev/Plugin/Assets/Effekseer/Test/EfkDynamicParameterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfkDynamicParameterScript : MonoBehaviour {
+
+	public GameObject TargetObject = null;
+
+	Effekseer.EffekseerEmitter emitter = null;
+
+	// Use this for initialization
+	void Start () {
+		emitter = GetComponent<Effekseer.EffekseerEmitter>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(emitter != null)
+		{
+			var pos = TargetObject.transform.position;
+			emitter.SetDynamicInputWithWorldPosition(ref pos);
+		}
+	}
+}
