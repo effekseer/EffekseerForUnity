b6a530ad34774fe49de7f1b0124d1826f2d86aa1
durswd <swda.durl@gmail.com>
2020-02-19 22:49:40 +0900

    Add Feature for URP

---
+++ Dev/Plugin/Assets/Effekseer/ScriptsExternal/EffekseerURPRenderPassFeature.cs
@@ -0,0 +1,58 @@
+﻿/*
+
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
+{
+    class EffekseerRenderPassURP : UnityEngine.Rendering.Universal.ScriptableRenderPass
+    {
+        RenderTargetIdentifier cameraColorTarget;
+        RenderTargetIdentifier cameraDepthTarget;
+
+        Effekseer.Internal.RenderTargetProperty prop = new Effekseer.Internal.RenderTargetProperty();
+
+        public EffekseerRenderPassURP(RenderTargetIdentifier cameraColorTarget, RenderTargetIdentifier cameraDepthTarget)
+        {
+            this.cameraColorTarget = cameraColorTarget;
+            this.cameraDepthTarget = cameraDepthTarget;
+            this.renderPassEvent = UnityEngine.Rendering.Universal.RenderPassEvent.AfterRenderingTransparents;
+
+            prop.colorTargetIdentifier = cameraColorTarget;
+            prop.depthTargetIdentifier = cameraDepthTarget;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref UnityEngine.Rendering.Universal.RenderingData renderingData)
+        {
+            if (Effekseer.EffekseerSystem.Instance == null) return;
+            prop.colorTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+
+            Effekseer.EffekseerSystem.Instance.renderer.Render(renderingData.cameraData.camera, prop, null);
+            var commandBuffer = Effekseer.EffekseerSystem.Instance.renderer.GetCameraCommandBuffer(renderingData.cameraData.camera);
+
+            if (commandBuffer != null)
+            {
+                context.ExecuteCommandBuffer(commandBuffer);
+                context.Submit();
+            }
+        }
+    }
+
+    EffekseerRenderPassURP m_ScriptablePass;
+
+    public override void Create()
+    {
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass = new EffekseerRenderPassURP(renderer.cameraColorTarget, renderer.cameraDepth);
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        renderer.EnqueuePass(m_ScriptablePass);
+
+    }
+}
+
+*/
+
---
+++ Tests/CopyToLWRP.bat
@@ -0,0 +1,9 @@
+robocopy ..\Dev\Plugin\Assets\Effekseer\Editor LWRPTests\Assets\Effekseer\Editor /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Materials LWRPTests\Assets\Effekseer\Materials /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Plugins LWRPTests\Assets\Effekseer\Plugins /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Resources LWRPTests\Assets\Effekseer\Resources /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Scripts LWRPTests\Assets\Effekseer\Scripts /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\ScriptsExternal LWRPTests\Assets\Effekseer\ScriptsExternal /s
+
+pause
+ 
