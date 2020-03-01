45400fcf0c3c34ce390c6177e0df1448fb665051
durswd <swda.durl@gmail.com>
2020-03-02 01:38:00 +0900

    Fix a script

--- ci/build.bat
+++ ci/build.bat
@@ -1,4 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
-cd C:\workdir\EffekseerForUnity\Dev\Cpp\windows
+call "C:\workdir\EffekseerForUnity\Dev\Cpp\windows\Build.bat"
 
-call Build.bat
