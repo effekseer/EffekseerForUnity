d5409cec9a13ffd80f06d59bd5d216bb697a9906
durswd <swda.durl@gmail.com>
2020-03-02 02:43:56 +0900

    fix

--- ci/build_windows_plugin.sh
+++ ci/build_windows_plugin.sh
@@ -8,7 +8,7 @@ ssh effekseer@192.168.56.102 <<EOC
 cd /mnt/c/workdir
 rd /s /q Effekseer
 git clone --recursive https://github.com/effekseer/Effekseer.git
-/mnt/c/Windows/System32/cmd.exe /c 'C:\workdir\build.bat' | nkf -w
+/mnt/c/Windows/System32/cmd.exe /c 'C:\workdir\EffekseerForUnity\Dev\Cpp\windows\Build.bat' | nkf -w
 EOC
 
 scp effekseer@192.168.56.102:/mnt/c/workdir/EffekseerForUnity/Dev/Cpp/build_x86/Release/EffekseerUnity.dll `pwd`/Dev/Plugin/Assets/Effekseer/Plugins/x86
