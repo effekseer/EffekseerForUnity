34b73f739381fa16f8b479e32ce710e935a03228
durswd <swda.durl@gmail.com>
2020-03-08 22:53:04 +0900

    Add tests

---
+++ Tests/CopyToURP.bat
@@ -0,0 +1,9 @@
+robocopy ..\Dev\Plugin\Assets\Effekseer\Editor URPTests\Assets\Effekseer\Editor /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Materials URPTests\Assets\Effekseer\Materials /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Plugins URPTests\Assets\Effekseer\Plugins /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Resources URPTests\Assets\Effekseer\Resources /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Scripts URPTests\Assets\Effekseer\Scripts /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\ScriptsExternal URPTests\Assets\Effekseer\ScriptsExternal /s
+
+pause
+ 
