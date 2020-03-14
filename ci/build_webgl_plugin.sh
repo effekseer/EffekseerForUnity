707e4d355fb1591be31eda06f0a478e59721e348
durswd <swda.durl@gmail.com>
2020-03-15 01:20:11 +0900

    Update ci

--- ci/build_webgl_plugin.sh
+++ ci/build_webgl_plugin.sh
@@ -1,8 +1,22 @@
 cd `dirname $0`
 source ~/emsdk/emsdk_env.sh
 cd ../Dev/Cpp/webgl
+
+mkdir build138
+cd build138
 emsdk activate sdk-1.38.11-64bit
-emcmake cmake 
+emcmake cmake ..
 make
-mkdir ../../Plugin/Assets/Effekseer/Plugins/WebGL/1.38.11-64bit/
-cp libEffekseerUnity.bc ../../Plugin/Assets/Effekseer/Plugins/WebGL/1.38.11-64bit/
+mkdir ../../../Plugin/Assets/Effekseer/Plugins/WebGL/1.38.11-64bit/
+cp libEffekseerUnity.bc ../../../Plugin/Assets/Effekseer/Plugins/WebGL/1.38.11-64bit/
+
+cd ..
+mkdir build137
+cd build137
+emsdk install sdk-1.37.3-64bit
+emsdk activate sdk-1.37.3-64bit
+emcmake cmake ..
+make
+mkdir ../../../Plugin/Assets/Effekseer/Plugins/WebGL/
+cp libEffekseerUnity.bc ../../../Plugin/Assets/Effekseer/Plugins/WebGL/
+
