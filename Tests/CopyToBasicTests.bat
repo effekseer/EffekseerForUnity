5f48dac62a8f710ddaad8c5bb0f9711d6d6af0d6
durswd <swda.durl@gmail.com>
2020-05-01 20:45:36 +0900

    Update tests

---
+++ Tests/CopyToBasicTests.bat
@@ -0,0 +1,9 @@
+robocopy ..\Dev\Plugin\Assets\Effekseer\Editor BasicTests\Assets\Effekseer\Editor /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Materials BasicTests\Assets\Effekseer\Materials /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Plugins BasicTests\Assets\Effekseer\Plugins /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Resources BasicTests\Assets\Effekseer\Resources /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\Scripts BasicTests\Assets\Effekseer\Scripts /s
+robocopy ..\Dev\Plugin\Assets\Effekseer\ScriptsExternal BasicTests\Assets\Effekseer\ScriptsExternal /s
+
+pause
+ 
