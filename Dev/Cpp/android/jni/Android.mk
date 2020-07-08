25db2f6adfbbb0671f4b302b6b8aa5ae69791b40
durswd <swda.durl@gmail.com>
2020-07-08 11:19:55 +0900

    Update Android.mk

--- Dev/Cpp/android/jni/Android.mk
+++ Dev/Cpp/android/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_ARM_MODE  := arm
 LOCAL_PATH      := $(NDK_PROJECT_PATH)
 LOCAL_MODULE    := libEffekseerUnity
-LOCAL_CFLAGS    := -O2 -D__EFFEKSEER_RENDERER_GLES2__ -D__EFFEKSEER_RENDERER_INTERNAL_LOADER__
+LOCAL_CFLAGS    := -O2 -D__EFFEKSEER_RENDERER_GLES2__ -D__EFFEKSEER_RENDERER_INTERNAL_LOADER__ -std=c++14
 LOCAL_LDLIBS    := -landroid -lEGL -lGLESv2
 
 LOCAL_C_INCLUDES += \
@@ -80,7 +80,9 @@ LOCAL_SRC_FILES += \
 	$(LIB_SRC_PATH)/Parameter/Effekseer.Parameters.cpp \
 	$(LIB_SRC_PATH)/SIMD/Effekseer.Mat43f.cpp \
 	$(LIB_SRC_PATH)/SIMD/Effekseer.Mat44f.cpp \
-	$(LIB_SRC_PATH)/SIMD/Effekseer.SIMDUtils.cpp
+	$(LIB_SRC_PATH)/SIMD/Effekseer.SIMDUtils.cpp \
+	$(LIB_SRC_PATH)/ForceField/ForceFields.cpp
+
 
 LIB_SRC_PATH := $(LOCAL_PATH)/../../../../Effekseer/Dev/Cpp/EffekseerMaterialCompiler
 LOCAL_SRC_FILES += \
