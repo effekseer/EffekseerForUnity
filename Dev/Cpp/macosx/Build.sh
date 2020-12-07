35835b00db363fa42ed3e8a8fef1d48c538cc2f5
durswd <swda.durl@gmail.com>
2020-12-07 21:23:39 +0900

    Fix workfrow and workaround for xcode12

--- Dev/Cpp/macosx/Build.sh
+++ Dev/Cpp/macosx/Build.sh
@@ -10,8 +10,11 @@ xcodebuild -target EffekseerUnity-iOS -sdk iphoneos -configuration Release build
 # build for iOS (Simulator)
 xcodebuild -target EffekseerUnity-iOS -sdk iphonesimulator -configuration Release build
 
+# workaround for xcode12
+lipo -remove arm64 build/Release-iphonesimulator/libEffekseerUnity.a -output build/Release-iphonesimulator/libEffekseerUnity_rmarm64.a
+
 # Create the Universal binary
-lipo -create build/Release-iphoneos/libEffekseerUnity.a build/Release-iphonesimulator/libEffekseerUnity.a -output libEffekseerUnity.a
+lipo -create build/Release-iphoneos/libEffekseerUnity.a build/Release-iphonesimulator/libEffekseerUnity_rmarm64.a -output libEffekseerUnity.a
 
 # Copy to PluginProject
 cp -rf build/Release/EffekseerUnity.bundle ../../Plugin/Assets/Effekseer/Plugins/
