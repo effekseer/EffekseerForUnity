357e36735a66c52e880bcd2e337cff4e2fe264ba
durswd <swda.durl@gmail.com>
2020-12-31 00:29:30 +0900

    fix

--- Dev/Cpp/android/jni/Android.mk
+++ Dev/Cpp/android/jni/Android.mk
@@ -127,7 +127,6 @@ LOCAL_SRC_FILES += \
 	$(LIB_SRC_PATH)/EffekseerRendererGL.Shader.cpp \
 	$(LIB_SRC_PATH)/EffekseerRendererGL.VertexArray.cpp \
 	$(LIB_SRC_PATH)/EffekseerRendererGL.VertexBuffer.cpp \
-	$(LIB_SRC_PATH)/EffekseerRendererGL.DeviceObjectCollection.cpp \
 	$(LIB_SRC_PATH)/GraphicsDevice.cpp
 
 include $(BUILD_SHARED_LIBRARY)
