764f4ea668dc854e02ab1617fa85a2cd519bbe8c
durswd <swda.durl@gmail.com>
2021-05-25 00:17:31 +0900

    Fix a bug where particle is not shown with SoftParticleParameter and left coordinate

--- Dev/Plugin/Assets/Effekseer/Scripts/EffekseerMaterialAsset.cs
+++ Dev/Plugin/Assets/Effekseer/Scripts/EffekseerMaterialAsset.cs
@@ -548,8 +548,9 @@ Cull[_Cull]
 			float2 zs = float2(backgroundZ * rescale.x + rescale.y, meshZ);
 
 			float2 depth = (zs * params.w - params.y) / (params.x - zs* params.z);
-
-			return min(max((depth.y - depth.x) / distance, 0.0), 1.0);
+			float dir = sign(depth.x);
+			depth *= dir;
+			return min(max((depth.x - depth.y) / distance, 0.0), 1.0);
 		}
 
 
