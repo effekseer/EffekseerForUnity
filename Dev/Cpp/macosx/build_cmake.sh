aac80637d63b45cccb8f62e1dca71dbba16165c2
swd <swda.durl@gmail.com>
2021-09-14 00:02:47 +0900

    Fix cmake for ios bitcode (#68)
    Fix cmake for ios bitcode

--- Dev/Cpp/macosx/build_cmake.sh
+++ Dev/Cpp/macosx/build_cmake.sh
@@ -1,31 +1,34 @@
-cd ..
+cd ../..
 cmake -B build_macosx -S . -G "Xcode" -D BUILD_EXAMPLES=OFF
 cd build_macosx
 cmake --build . --config Release
+cmake --install . --prefix ./Install
 
 cd ..
 cmake -B build_ios -S . -G "Xcode" -D BUILD_EXAMPLES=OFF -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=iphoneos -D BUILD_FOR_IOS=ON
 cd build_ios
 cmake --build . --config Release
+cmake --install . --prefix ./Install
 
 cd ..
 cmake -B build_ios_sim -S . -G "Xcode" -D BUILD_EXAMPLES=OFF -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_SYSROOT=iphonesimulator -D BUILD_FOR_IOS_SIM=ON
 cd build_ios_sim
 cmake --build . --config Release
+cmake --install . --prefix ./Install
 
-cd ../macosx
+cd ../Cpp/macosx
 
 # workaround for xcode12
-lipo -remove arm64 ../build_ios_sim/Release-iphonesimulator/libEffekseerUnity.a -output ../build_ios_sim/Release-iphonesimulator/libEffekseerUnity_rmarm64.a
-lipo -remove arm64 ../build_ios_sim/Install/Effekseer/lib/libEffekseerRendererCommon.a -output ../build_ios_sim/Install/Effekseer/lib/libEffekseerRendererCommon_rmarm64.a
-lipo -remove arm64 ../build_ios_sim/Install/Effekseer/lib/libEffekseer.a -output ../build_ios_sim/Install/Effekseer/lib/libEffekseer_rmarm64.a
+lipo -remove arm64 ../../build_ios_sim/Install/lib/libEffekseerUnity.a -output ../../build_ios_sim/Install/lib/libEffekseerUnity_rmarm64.a
+lipo -remove arm64 ../../build_ios_sim/Install/lib/libEffekseerRendererCommon.a -output ../../build_ios_sim/Install/lib/libEffekseerRendererCommon_rmarm64.a
+lipo -remove arm64 ../../build_ios_sim/Install/lib/libEffekseer.a -output ../../build_ios_sim/Install/lib/libEffekseer_rmarm64.a
 
 # Create the Universal binary
-libtool -static -o libEffekseerUnity_ios.a ../build_ios/Install/Effekseer/lib/libEffekseer.a ../build_ios/Install/Effekseer/lib/libEffekseerRendererCommon.a ../build_ios/Release-iphoneos/libEffekseerUnity.a
-libtool -static -o libEffekseerUnity_ios_sim.a ../build_ios_sim/Install/Effekseer/lib/libEffekseer_rmarm64.a ../build_ios_sim/Install/Effekseer/lib/libEffekseerRendererCommon_rmarm64.a ../build_ios_sim/Release-iphonesimulator/libEffekseerUnity_rmarm64.a
+libtool -static -o libEffekseerUnity_ios.a ../../build_ios/Install/lib/libEffekseer.a ../../build_ios/Install/lib/libEffekseerRendererCommon.a ../../build_ios/Install/lib/libEffekseerUnity.a
+libtool -static -o libEffekseerUnity_ios_sim.a ../../build_ios_sim/Install/lib/libEffekseer_rmarm64.a ../../build_ios_sim/Install/lib/libEffekseerRendererCommon_rmarm64.a ../../build_ios_sim/Install/lib/libEffekseerUnity_rmarm64.a
 
 lipo -create libEffekseerUnity_ios.a libEffekseerUnity_ios_sim.a -output libEffekseerUnity.a
 
 # Copy to PluginProject
-cp -rf ../build_macosx/Release/EffekseerUnity.bundle ../../Plugin/Assets/Effekseer/Plugins/
+cp -rf ../../build_macosx/Install/lib/EffekseerUnity.bundle ../../Plugin/Assets/Effekseer/Plugins/
 cp libEffekseerUnity.a ../../Plugin/Assets/Effekseer/Plugins/iOS/
