cb10af6de515e9179ef6d4aca7e851fb1dba5731
fum1h1ro <fumihiro@gmail.com>
2021-09-13 09:31:32 +0900

    Revert "fix: supress URP warning message"
    This reverts commit 95ae40e38c788b8d0c7c27a1fd797bc6011cdafa.

--- Dev/Plugin/Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
+++ Dev/Plugin/Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
@@ -8,13 +8,25 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 {
 	class EffekseerRenderPassURP : UnityEngine.Rendering.Universal.ScriptableRenderPass
 	{
+		RenderTargetIdentifier cameraColorTarget;
+		RenderTargetIdentifier cameraDepthTarget;
+
 		Effekseer.Internal.RenderTargetProperty prop = new Effekseer.Internal.RenderTargetProperty();
 
-		public EffekseerRenderPassURP()
+		public EffekseerRenderPassURP(RenderTargetIdentifier cameraColorTarget, RenderTargetIdentifier cameraDepthTarget)
 		{
+			// HACK
+			bool isValidDepth = !cameraDepthTarget.ToString().Contains("-1");
+
+			this.cameraColorTarget = cameraColorTarget;
+			prop.colorTargetIdentifier = cameraColorTarget;
 			this.renderPassEvent = UnityEngine.Rendering.Universal.RenderPassEvent.AfterRenderingTransparents;
-			prop.colorTargetIdentifier = colorAttachment;
-			prop.depthTargetIdentifier = depthAttachment;
+
+			if(isValidDepth)
+			{
+				this.cameraDepthTarget = cameraDepthTarget;
+				prop.depthTargetIdentifier = cameraDepthTarget;
+			}
 		}
 
 		public override void Execute(ScriptableRenderContext context, ref UnityEngine.Rendering.Universal.RenderingData renderingData)
@@ -43,7 +55,7 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 
 	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
 	{
-		m_ScriptablePass = new EffekseerRenderPassURP();
+		m_ScriptablePass = new EffekseerRenderPassURP(renderer.cameraColorTarget, renderer.cameraDepth);
 		m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
 		renderer.EnqueuePass(m_ScriptablePass);
 
