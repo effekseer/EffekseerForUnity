24dc364f9a54d8cfee38389babcef0708d21fb3e
durswd <swda.durl@gmail.com>
2022-12-18 01:25:44 +0900

    Add release option

--- Dev/Cpp/webgl/Build_1.38.11.bat
+++ Dev/Cpp/webgl/Build_1.38.11.bat
@@ -3,7 +3,7 @@
 call emsdk activate sdk-fastcomp-tag-1.38.11-64bit
 
 rem emscripten configuration
-call emcmake cmake -G "MinGW Makefiles"
+call emcmake cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
 
 rem build
 mingw32-make
--- Dev/Cpp/webgl/Build_2.0.19.bat
+++ Dev/Cpp/webgl/Build_2.0.19.bat
@@ -3,7 +3,7 @@
 call emsdk activate sdk-2.0.19-64bit
 
 rem emscripten configuration
-call emcmake cmake -G "MinGW Makefiles"
+call emcmake cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
 
 rem build
 mingw32-make
