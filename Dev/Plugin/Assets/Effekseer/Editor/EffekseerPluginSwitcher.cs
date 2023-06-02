78801761092af1f41a8c968ff247358adab71c7c
swd <swda.durl@gmail.com>
2023-06-02 18:42:24 +0900

    Support WebGL 3.1.8 (#99)
    * Support WebGL 3.1.8

--- Dev/Plugin/Assets/Effekseer/Editor/EffekseerPluginSwitcher.cs
+++ Dev/Plugin/Assets/Effekseer/Editor/EffekseerPluginSwitcher.cs
@@ -10,7 +10,9 @@ public class EffekseerPluginSwitcher
 {
 	static readonly string tempFilePath = "Temp/EffekseerPluginSwitcher";
 
-#if UNITY_2021_2_OR_NEWER
+#if UNITY_2022_2_OR_NEWER
+	static readonly string bcPath = "WebGL/3.1.8-64bit/libEffekseerUnity.bc";
+#elif UNITY_2021_2_OR_NEWER
 	static readonly string bcPath = "WebGL/2.0.19-64bit/libEffekseerUnity.bc";
 #else
 	static readonly string bcPath = "WebGL/libEffekseerUnity.bc";
