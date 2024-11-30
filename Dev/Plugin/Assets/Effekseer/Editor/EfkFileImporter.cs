a6d78a1f7ee9de31aeabab369469b8907a1bda90
durswd <swda.durl@gmail.com>
2024-11-30 11:06:01 +0900

    Fix a bug where Unity cannot build the project

--- Dev/Plugin/Assets/Effekseer/Editor/EfkFileImporter.cs
+++ Dev/Plugin/Assets/Effekseer/Editor/EfkFileImporter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -145,3 +146,4 @@ namespace Effekseer.Editor
 		}
 	}
 }
+#endif
