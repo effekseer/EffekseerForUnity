2110985e96881a167687b75d077a92cfe08f54c1
durswd <swda.durl@gmail.com>
2025-12-12 17:24:32 +0900

    Add a flag

--- Dev/Plugin/Assets/Effekseer/Scripts/EffekseerSystem.cs
+++ Dev/Plugin/Assets/Effekseer/Scripts/EffekseerSystem.cs
@@ -301,7 +301,7 @@ namespace Effekseer
 				Debug.LogError("[Effekseer] EffekseerSystem instance is already found.");
 			}
 
-#if (UNITY_WEBGL || UNITY_IOS || UNITY_SWITCH) && !UNITY_EDITOR
+#if (UNITY_WEBGL || UNITY_IOS || UNITY_SWITCH || UNITY_SWITCH2) && !UNITY_EDITOR
 			Plugin.RegisterPlugin();
 #endif
 
