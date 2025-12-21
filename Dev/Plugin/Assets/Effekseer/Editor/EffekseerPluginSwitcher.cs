272a7143b2870b96fe119f64cd5b2bcb381757e0
durswd <swda.durl@gmail.com>
2025-12-21 18:14:47 +0900

    fix

--- Dev/Plugin/Assets/Effekseer/Editor/EffekseerPluginSwitcher.cs
+++ Dev/Plugin/Assets/Effekseer/Editor/EffekseerPluginSwitcher.cs
@@ -17,7 +17,13 @@ public class EffekseerPluginSwitcher
 #elif UNITY_2021_2_OR_NEWER
 	static readonly string bcPath = "WebGL/2.0.19-64bit/libEffekseerUnity.bc";
 #else
-	static readonly string bcPath = "WebGL/libEffekseerUnity.bc";
+	static readonly string bcPath = string.Empty;
+#endif
+	static readonly bool webglSupported =
+#if UNITY_2021_2_OR_NEWER
+		true;
+#else
+		false;
 #endif
 
 	static EffekseerPluginSwitcher()
@@ -37,7 +43,7 @@ public class EffekseerPluginSwitcher
 				var processed = new List<string>();
 				foreach (var importer in importers)
 				{
-					var enabled = importer.assetPath.Contains(bcPath);
+					var enabled = webglSupported && importer.assetPath.Contains(bcPath);
 					importer.SetCompatibleWithPlatform(BuildTarget.WebGL, enabled);
 					processed.Add(importer.assetPath + " : " + enabled.ToString());
 				}
