6e4fe4f5d9f5d964c5b134d1e5d2bd4d0731a122
durswd <swda.durl@gmail.com>
2026-02-23 17:54:11 +0900

    fix

--- Dev/Cpp/linux/build_cmake.sh
+++ Dev/Cpp/linux/build_cmake.sh
@@ -3,9 +3,11 @@ set -eu
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 cd "$SCRIPT_DIR/../.."
 
-mkdir -p build_linux
-cmake -B build_linux -S . -DCMAKE_BUILD_TYPE=Release -D BUILD_EXAMPLES=OFF -D USE_OPENAL=OFF
-cmake --build build_linux --config Release --parallel 4
+BUILD_DIR=${BUILD_DIR:-build_linux}
+
+mkdir -p "$BUILD_DIR"
+cmake -B "$BUILD_DIR" -S . -DCMAKE_BUILD_TYPE=Release -D BUILD_EXAMPLES=OFF -D USE_OPENAL=OFF
+cmake --build "$BUILD_DIR" --config Release --parallel 4
 
 mkdir -p Plugin/Assets/Effekseer/Plugins/Linux/x86_64
-cp -f build_linux/Cpp/libEffekseerUnity.so Plugin/Assets/Effekseer/Plugins/Linux/x86_64/
+cp -f "$BUILD_DIR"/Cpp/libEffekseerUnity.so Plugin/Assets/Effekseer/Plugins/Linux/x86_64/
