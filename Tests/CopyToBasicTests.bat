45f3d44067b84eec08be1897b21fb0c478466f65
durswd <swda.durl@gmail.com>
2026-03-04 01:02:33 +0900

    Update tests

--- Tests/CopyToBasicTests.bat
+++
@@ -1,9 +0,0 @@
-robocopy ..\Dev\Plugin\Assets\Effekseer\Editor BasicTests\Assets\Effekseer\Editor /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Materials BasicTests\Assets\Effekseer\Materials /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Plugins BasicTests\Assets\Effekseer\Plugins /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Resources BasicTests\Assets\Effekseer\Resources /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Scripts BasicTests\Assets\Effekseer\Scripts /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\External BasicTests\Assets\Effekseer\External /s
-
-pause
- 
--- Tests/CopyToURP.bat
+++
@@ -1,9 +0,0 @@
-robocopy ..\Dev\Plugin\Assets\Effekseer\Editor URPTests\Assets\Effekseer\Editor /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Materials URPTests\Assets\Effekseer\Materials /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Plugins URPTests\Assets\Effekseer\Plugins /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Resources URPTests\Assets\Effekseer\Resources /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\Scripts URPTests\Assets\Effekseer\Scripts /s
-robocopy ..\Dev\Plugin\Assets\Effekseer\External URPTests\Assets\Effekseer\External /s
-
-pause
- 
