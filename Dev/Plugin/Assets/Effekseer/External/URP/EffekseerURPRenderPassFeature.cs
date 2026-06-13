b960f095e7e8668df20756944101985d3e3e19d8
durswd <swda.durl@gmail.com>
2026-06-13 11:49:18 +0900

    Fix a flag in URP

--- Dev/Plugin/Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
+++ Dev/Plugin/Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
@@ -195,7 +195,7 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 				passData.depthTexture = resourceData.activeDepthTexture;
 				if (passData.depthTexture.IsValid())
 				{
-					builder.UseTexture(passData.depthTexture, AccessFlags.Write);
+					builder.UseTexture(passData.depthTexture, AccessFlags.ReadWrite);
 				}
 				PrepareRenderTargetProperty(passData.prop, cameraData.cameraTargetDescriptor, cameraData.requiresDepthTexture, xrRendering);
 
